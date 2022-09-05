c3e14edfef921edaade4f7363c4ed39bc256cb0b
ThomasKroes <tkroes81@gmail.com>
2022-09-05 10:19:10 +0200

    Feature/multi input plugin (#1)
    * Initial commit, working version
    
    * Update plugin trigger action API
    
    * Work on plugin trigger API
    
    * Refactor plugin factory
    
    * Work on conversion
    
    * Work on actions
    
    * End of day commit, working version
    
    Co-authored-by: Thomas Kroes <t.kroes@lumc.nl>

---
+++ src/PointDataConversionPlugin.cpp
@@ -0,0 +1,201 @@
+#include "PointDataConversionPlugin.h"
+
+#include <PointData.h>
+
+#include <actions/PluginTriggerAction.h>
+
+#include <cmath>
+
+#include <QDebug>
+
+Q_PLUGIN_METADATA(IID "nl.BioVault.PointDataConversionPlugin")
+
+using namespace hdps;
+
+const QMap<PointDataConversionPlugin::Type, QString> PointDataConversionPlugin::types = QMap<PointDataConversionPlugin::Type, QString>({
+    { PointDataConversionPlugin::Type::Log2, "log2(value+1)" },
+    { PointDataConversionPlugin::Type::ArcSin, "arcsin(value/factor)" }
+});
+
+PointDataConversionPlugin::PointDataConversionPlugin(const PluginFactory* factory) :
+    TransformationPlugin(factory),
+    _type(Type::ArcSin)
+{
+}
+
+void PointDataConversionPlugin::transform()
+{
+    for (auto inputDataset : getInputDatasets()) {
+        auto points = Dataset<Points>(inputDataset);
+
+        if (!points.isValid())
+            continue;
+
+        points->setLocked(true);
+        {
+            QApplication::processEvents();
+
+            points->getDataHierarchyItem().setTaskName("Converting");
+            points->getDataHierarchyItem().setTaskRunning();
+            points->getDataHierarchyItem().setTaskDescription(QString("%1 conversion").arg(getTypeName(_type)));
+
+            points->visitData([this, &points](auto pointData) {
+                std::uint32_t noPointsProcessed = 0;
+
+                for (auto point : pointData) {
+                    for (std::int32_t dimensionIndex = 0; dimensionIndex < points->getNumDimensions(); dimensionIndex++) {
+                        switch (_type)
+                        {
+                            case PointDataConversionPlugin::Type::Log2:
+                                point[dimensionIndex] = log2(point[dimensionIndex] + 1);
+                                break;
+
+                            case PointDataConversionPlugin::Type::ArcSin:
+                                point[dimensionIndex] = asinh(point[dimensionIndex] / 5.0f);
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+
+                    ++noPointsProcessed;
+
+                    if (noPointsProcessed % 1000 == 0) {
+                        points->getDataHierarchyItem().setTaskProgress(static_cast<float>(noPointsProcessed) / static_cast<float>(points->getNumPoints()));
+                        
+                        QApplication::processEvents();
+                    }
+                }
+            });
+
+            points->getDataHierarchyItem().setTaskProgress(1.0f);
+            points->getDataHierarchyItem().setTaskFinished();
+
+            _core->notifyDatasetChanged(points);
+        }
+        points->setLocked(false);
+    }
+}
+
+PointDataConversionPlugin::Type PointDataConversionPlugin::getType() const
+{
+    return _type;
+}
+
+void PointDataConversionPlugin::setType(const Type& type)
+{
+    if (type == _type)
+        return;
+
+    _type = type;
+}
+
+QString PointDataConversionPlugin::getTypeName(const Type& type)
+{
+    return types[type];
+}
+
+PointDataConversionPluginFactory::PointDataConversionPluginFactory() :
+    _arcSinFactorAction(this, "Factor", 1.0f, 10.0f, 5.0f, 5.0f, 1)
+{
+}
+
+PointDataConversionPlugin* PointDataConversionPluginFactory::produce()
+{
+    return new PointDataConversionPlugin(this);
+}
+
+PluginTriggerActions PointDataConversionPluginFactory::getPluginTriggerActions(const hdps::Datasets& datasets) const
+{
+    PluginTriggerActions pluginTriggerActions;
+
+    const auto numberOfDatasets = datasets.count();
+
+    if (PluginFactory::areAllDatasetsOfTheSameType(datasets, PointType)) {
+        if (numberOfDatasets >= 1 && datasets.first()->getDataType() == PointType) {
+            const auto addPluginTriggerAction = [this, &pluginTriggerActions, datasets](const PointDataConversionPlugin::Type& type) -> void {
+                const auto typeName = PointDataConversionPlugin::getTypeName(type);
+
+                auto pluginTriggerAction = createPluginTriggerAction(typeName, QString("Perform %1 data conversion").arg(typeName), datasets);
+
+                connect(pluginTriggerAction, &QAction::triggered, [this, datasets, type]() -> void {
+                    for (auto dataset : datasets) {
+                        auto pluginInstance = dynamic_cast<PointDataConversionPlugin*>(Application::core()->requestPlugin(getKind()));
+
+                        pluginInstance->setInputDatasets(datasets);
+                        pluginInstance->setType(type);
+                        pluginInstance->transform();
+                    }
+                });
+
+                pluginTriggerActions << pluginTriggerAction;
+            };
+
+            addPluginTriggerAction(PointDataConversionPlugin::Type::Log2);
+            addPluginTriggerAction(PointDataConversionPlugin::Type::ArcSin);
+        }
+    }
+
+    return pluginTriggerActions;
+}
+
+PluginTriggerActions PointDataConversionPluginFactory::getPluginTriggerActions(const hdps::DataTypes& dataTypes) const
+{
+    PluginTriggerActions pluginTriggerActions;
+
+    if (dataTypes.count(PointType) == dataTypes.count()) {
+        const auto addPluginTriggerAction = [this, &pluginTriggerActions](const PointDataConversionPlugin::Type& type) -> void {
+            const auto typeName = PointDataConversionPlugin::getTypeName(type);
+
+            auto pluginTriggerAction = createPluginTriggerAction(typeName, QString("Perform %1 data conversion").arg(typeName), Datasets());
+
+            pluginTriggerAction->setConfigurationAction(const_cast<PointDataConversionPluginFactory*>(this)->getConfigurationAction(type));
+
+            connect(pluginTriggerAction, &QAction::triggered, this, [this, type, pluginTriggerAction]() -> void {
+                for (auto dataset : pluginTriggerAction->getDatasets()) {
+                    auto pluginInstance = dynamic_cast<PointDataConversionPlugin*>(Application::core()->requestPlugin(getKind()));
+
+                    pluginInstance->setInputDatasets(pluginTriggerAction->getDatasets());
+                    pluginInstance->setType(type);
+                    pluginInstance->transform();
+                }
+            });
+
+            pluginTriggerActions << pluginTriggerAction;
+        };
+
+        addPluginTriggerAction(PointDataConversionPlugin::Type::Log2);
+        addPluginTriggerAction(PointDataConversionPlugin::Type::ArcSin);
+    }
+
+    return pluginTriggerActions;
+}
+
+WidgetAction* PointDataConversionPluginFactory::getConfigurationAction(const PointDataConversionPlugin::Type& type)
+{
+    const auto createGroupAction = [this](const WidgetActions& widgetActions) -> GroupAction* {
+        auto groupAction = new GroupAction(this);
+
+        groupAction->setText("Settings");
+        groupAction->setToolTip("Data conversion settings");
+        groupAction->setLabelSizingType(GroupAction::LabelSizingType::Auto);
+        groupAction->setActions(widgetActions);
+
+        return groupAction;
+    };
+
+    switch (type)
+    {
+        case PointDataConversionPlugin::Type::Log2:
+            return nullptr;
+
+        case PointDataConversionPlugin::Type::ArcSin:
+            return createGroupAction({ &_arcSinFactorAction });
+
+        default:
+            break;
+    }
+
+    return nullptr;
+}
---
+++ src/PointDataConversionPlugin.h
@@ -0,0 +1,118 @@
+#pragma once
+
+#include <TransformationPlugin.h>
+
+#include <Dataset.h>
+
+using namespace hdps::plugin;
+using namespace hdps::gui;
+using namespace hdps::util;
+
+class QLabel;
+
+/**
+ * Point data conversion plugin class
+ *
+ * @author Thomas Kroes
+ */
+class PointDataConversionPlugin : public TransformationPlugin
+{
+    Q_OBJECT
+
+public:
+
+    /** Point data conversion type */
+    enum class Type {
+        Log2,       /** log2(value+1) */
+        ArcSin      /** asinh(value/factor) */
+    };
+
+    static const QMap<Type, QString> types;
+
+public:
+
+    /**
+     * Constructor
+     * @param factory Pointer to the plugin factory
+     */
+    PointDataConversionPlugin(const PluginFactory* factory);
+
+    /** Destructor */
+    ~PointDataConversionPlugin() override = default;
+    
+    /** Initialization is called when the plugin is first instantiated. */
+    void init() override {};
+
+    /** Performs the data transformation */
+    void transform() override;
+
+    /**
+    /**
+     * Get point data conversion type
+     * @return Point data conversion type
+     */
+    Type getType() const;
+
+    /**
+     * Set point data conversion type
+     * @param type Point data conversion type
+     */
+    void setType(const Type& type);
+
+    /**
+     * Get string representation of type enum
+     * @param type Point data conversion type
+     * @return Type name
+     */
+    static QString getTypeName(const Type& type);
+
+private:
+    Type    _type;      /** Data conversion type */
+};
+
+/**
+ * Point data conversion plugin factory class
+ *
+ * @author Thomas Kroes
+ */
+class PointDataConversionPluginFactory : public TransformationPluginFactory
+{
+    Q_INTERFACES(hdps::plugin::TransformationPluginFactory hdps::plugin::PluginFactory)
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID   "nl.BioVault.PointDataConversionPlugin"
+                      FILE  "PointDataConversionPlugin.json")
+
+public:
+
+    /** Default constructor */
+    PointDataConversionPluginFactory();
+
+    /** Destructor */
+    ~PointDataConversionPluginFactory() override {}
+    
+    /** Creates an instance of the point data conversion plugin */
+    PointDataConversionPlugin* produce() override;
+
+    /**
+     * Get plugin trigger actions given \p datasets
+     * @param datasets Vector of input datasets
+     * @return Vector of plugin trigger actions
+     */
+    PluginTriggerActions getPluginTriggerActions(const hdps::Datasets& datasets) const override;
+
+    /**
+     * Get plugin trigger actions given \p dataTypes
+     * @param datasetTypes Vector of input data types
+     * @return Vector of plugin trigger actions
+     */
+    PluginTriggerActions getPluginTriggerActions(const hdps::DataTypes& dataTypes) const override;
+
+    /**
+     * Get configuration action for \p type
+     * @return Pointer to configuration action (may be null)
+     */
+    WidgetAction* getConfigurationAction(const PointDataConversionPlugin::Type& type);
+
+private:
+    DecimalAction   _arcSinFactorAction;    /** Factor for arcsin(value/factor) conversion */
+};
