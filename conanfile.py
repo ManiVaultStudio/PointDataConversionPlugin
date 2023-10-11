843659b033f3c321b025fd4a3fea53c4d0a19d8c
Julian Thijssen <julianthijssen@gmail.com>
2023-10-11 16:46:56 +0200

    Add MV_INSTALL_DIR variable through conan

--- conanfile.py
+++ conanfile.py
@@ -91,6 +91,16 @@ class PointDataConversionPluginConan(ConanFile):
         if self.settings.os == "Linux" or self.settings.os == "Macos":
             tc.variables["CMAKE_CXX_STANDARD_REQUIRED"] = "ON"
         tc.variables["CMAKE_PREFIX_PATH"] = qt_root
+        
+        # Set the installation directory for ManiVault based on the MV_INSTALL_DIR environment variable
+        # or if none is specified, set it to the build/install dir.
+        if not os.environ.get("MV_INSTALL_DIR", None):
+            os.environ["MV_INSTALL_DIR"] = os.path.join(self.build_folder, "install")
+        print("MV_INSTALL_DIR: ", os.environ["MV_INSTALL_DIR"])
+        self.install_dir = pathlib.Path(os.environ["MV_INSTALL_DIR"]).as_posix()
+        # Give the installation directory to CMake
+        tc.variables["MV_INSTALL_DIR"] = self.install_dir
+        
         tc.generate()
 
     def _configure_cmake(self):
@@ -101,11 +111,6 @@ class PointDataConversionPluginConan(ConanFile):
 
     def build(self):
         print("Build OS is : ", self.settings.os)
-        # If the user has no preference in HDPS_INSTALL_DIR simply set the install dir
-        if not os.environ.get("HDPS_INSTALL_DIR", None):
-            os.environ["HDPS_INSTALL_DIR"] = os.path.join(self.build_folder, "install")
-        print("HDPS_INSTALL_DIR: ", os.environ["HDPS_INSTALL_DIR"])
-        self.install_dir = os.environ["HDPS_INSTALL_DIR"]
 
         hdps_pkg_root = self.deps_cpp_info["hdps-core"].rootpath
         print("Install dir type: ", self.install_dir)
